2776ee6ef0548026e017f199b3b4a201c0db69ac
John Khoo <john_khoo@u.nus.edu>
2021-04-08 21:25:36 +0800

    Add helper methods and generalise some functions

--- qalpha.cpp
+++ qalpha.cpp
@@ -8,6 +8,9 @@ AlphaDelegate::AlphaDelegate(alpha_id_t root_id) {
     addNode(nullptr, 0);
     addNode(root, root_id);
 }
+AlphaDelegate::~AlphaDelegate() {
+    alpha_delnode(root);
+}
 
 alpha_id_t AlphaDelegate::addNode(struct alpha_node *node) {
     if (!node) {
@@ -22,7 +25,6 @@ alpha_id_t AlphaDelegate::addNode(struct alpha_node *node) {
     addNode(node, id);
     return id;
 }
-
 void AlphaDelegate::addNode(struct alpha_node *node, alpha_id_t id) {
     if (id_node.find(id) != id_node.end() || node_id.find(node) != node_id.end()) {
         return;
@@ -31,25 +33,41 @@ void AlphaDelegate::addNode(struct alpha_node *node, alpha_id_t id) {
     id_node.insert(id, node);
     node_id.insert(node, id);
 }
+void AlphaDelegate::reset(void) {
+    alpha_id_t root_id = node_id[root];
+    id_node.clear();
+    node_id.clear();
+    alpha_delnode(root);
+
+    alpha_ret_t ret;
+    root = alpha_makenode(nullptr, nullptr, ALPHA_TYPE_AND, &ret);
+    addNode(nullptr, 0);
+    addNode(root, root_id);
+}
 
 void AlphaDelegate::remId(alpha_id_t id) {
     struct alpha_node *node = *(id_node.find(id));
     id_node.remove(id);
     node_id.remove(node);
 }
-
 void AlphaDelegate::remNode(alpha_node *node) {
     alpha_id_t id = *(node_id.find(node));
     node_id.remove(node);
     id_node.remove(id);
 }
-
 alpha_id_t AlphaDelegate::getId(struct alpha_node *node) {
-    return *(node_id.find(node));
+    auto it = node_id.find(node);
+    if (it == node_id.end()) {
+        return 0;
+    }
+    return *it;
 }
-
 struct alpha_node *AlphaDelegate::getNode(alpha_id_t id){
-    return *(id_node.find(id));
+    auto it = id_node.find(id);
+    if (it == id_node.end()) {
+        return nullptr;
+    }
+    return *it;
 }
 
 QAlpha::QAlpha(QObject *parent) :
@@ -58,23 +76,153 @@ QAlpha::QAlpha(QObject *parent) :
     clipboard_root = clipboardDelegate.root;
 }
 
-void QAlpha::stage_clear(void) { }
-void QAlpha::clip_clear(void) { }
+void QAlpha::_clear(AlphaDelegate *delegatep, struct alpha_node **rootp) {
+    delegatep->reset();
+    *rootp = delegatep->root;
+}
+void QAlpha::_register(AlphaDelegate *delegatep, struct alpha_node *node) {
+    delegatep->addNode(node);
+    for (size_t i = 0; i < node->children.num_sibs; ++i) {
+        struct alpha_node *child = node->children.sibs[i];
+        _register(delegatep, child);
+    }
+}
+QString QAlpha::_get_node(AlphaDelegate *delegatep, alpha_id_t id) {
+    struct alpha_node *node = delegatep->getNode(id);
+    if (node->name) {
+        return QString(node->name);
+    }
+    return QString("");
+}
+QList<int> QAlpha::_get_children(AlphaDelegate *delegatep, alpha_id_t id) {
+    struct alpha_node *node = delegatep->getNode(id);
+    return _get_children(delegatep, node);
+}
+QList<int> QAlpha::_get_children(AlphaDelegate *delegatep, struct alpha_node *node) {
+    QList<int> childIds;
+    for (size_t i = 0; i < node->children.num_sibs; ++i) {
+        int id = delegatep->getId(node->children.sibs[i]);
+        childIds.append(id);
+    }
+    return childIds;
+}
+QList<int> QAlpha::_get_descendants(AlphaDelegate *delegatep, alpha_id_t id) {
+    struct alpha_node *node = delegatep->getNode(id);
+    return _get_descendants(delegatep, node);
+}
+QList<int> QAlpha::_get_descendants(AlphaDelegate *delegatep, struct alpha_node *node) {
+    QList<int> descendants;
+    for (size_t i = 0; i < node->children.num_sibs; ++i) {
+        struct alpha_node *child = node->children.sibs[i];
+        int id = delegatep->getId(child);
+        descendants += id;
+        descendants += _get_descendants(delegatep, child);
+    }
+    return descendants;
+}
 
-int QAlpha::prf_insert(alpha_id_t target, alpha_id_t) { }
-int QAlpha::prf_delete(alpha_id_t target) { }
-int QAlpha::prf_addcut(alpha_id_t target) { }
-int QAlpha::prf_remcut(alpha_id_t target) { }
-int QAlpha::prf_cut(alpha_id_t target) { }
-int QAlpha::prf_yank(alpha_id_t target) { }
+void QAlpha::stage_clear(void) {
+    _clear(&stageDelegate, &stage_root);
+}
+void QAlpha::clip_clear(void) {
+    _clear(&clipboardDelegate, &clipboard_root);
+}
+QString QAlpha::stage_get_node(alpha_id_t id) {
+    return _get_node(&stageDelegate, id);
+}
+QString QAlpha::clip_get_node(alpha_id_t id) {
+    return _get_node(&clipboardDelegate, id);
+}
+QList<int> QAlpha::stage_get_children(alpha_id_t id) {
+    return _get_children(&stageDelegate, id);
+}
+QList<int> QAlpha::clip_get_children(alpha_id_t id) {
+    return _get_children(&clipboardDelegate, id);
+}
 
-int QAlpha::hypo_insert(alpha_id_t target, alpha_id_t) { }
-int QAlpha::hypo_delete(alpha_id_t target) { }
-int QAlpha::hypo_addcut(alpha_id_t target) { }
-int QAlpha::hypo_remcut(alpha_id_t target) { }
+int QAlpha::prf_insert(alpha_id_t target, alpha_id_t content) {
+    struct alpha_node *tgtnode = stageDelegate.getNode(target);
+    struct alpha_node *contnode = clipboardDelegate.getNode(content);
 
-int QAlpha::clip_insert(alpha_id_t target, alpha_id_t) { }
-int QAlpha::clip_delete(alpha_id_t target) { }
-int QAlpha::clip_addcut(alpha_id_t target) { }
-int QAlpha::clip_remcut(alpha_id_t target) { }
+    if (alpha_prfinsert(tgtnode, contnode) == ALPHA_RET_OK) {
+        /* rely on internal error checking to ignore existing nodes */
+        _register(&stageDelegate, tgtnode);
+        return retOK;
+    } else {
+        return retINVALID;
+    }
+}
 
+int QAlpha::prf_delete(alpha_id_t target) {
+    struct alpha_node *tgtnode = stageDelegate.getNode(target);
+    QList<int> to_delete = _get_descendants(&stageDelegate, tgtnode);
+    to_delete += target;
+
+    if (alpha_prferase(tgtnode) == ALPHA_RET_OK) {
+        for (auto it = to_delete.begin(); it < to_delete.end(); ++it) {
+            stageDelegate.remId(*it);
+        }
+        return retOK;
+    } else {
+        return retINVALID;
+    }
+}
+
+int QAlpha::prf_addcut(alpha_id_t target) {
+    struct alpha_node *tgtnode = stageDelegate.getNode(target);
+}
+
+int QAlpha::prf_remcut(alpha_id_t target) {
+    struct alpha_node *tgtnode = stageDelegate.getNode(target);
+}
+
+int QAlpha::prf_cut(alpha_id_t target) {
+    struct alpha_node *tgtnode = stageDelegate.getNode(target);
+}
+
+int QAlpha::prf_yank(alpha_id_t target) {
+    struct alpha_node *tgtnode = stageDelegate.getNode(target);
+}
+
+int QAlpha::_hypo_insert(AlphaDelegate *delegatep, alpha_id_t target, alpha_id_t content) {
+    struct alpha_node *tgtnode = delegatep->getNode(target);
+    struct alpha_node *contnode = delegatep->getNode(content);
+}
+
+int QAlpha::_hypo_delete(AlphaDelegate *delegatep, alpha_id_t target) {
+    struct alpha_node *tgtnode = delegatep->getNode(target);
+}
+
+int QAlpha::_hypo_addcut(AlphaDelegate *delegatep, alpha_id_t target) {
+    struct alpha_node *tgtnode = delegatep->getNode(target);
+}
+
+int QAlpha::_hypo_remcut(AlphaDelegate *delegatep, alpha_id_t target) {
+    struct alpha_node *tgtnode = delegatep->getNode(target);
+}
+
+int QAlpha::hypo_insert(alpha_id_t target, alpha_id_t content) {
+    return _hypo_insert(&stageDelegate, target, content);
+}
+int QAlpha::hypo_delete(alpha_id_t target) {
+    return _hypo_delete(&stageDelegate, target);
+}
+int QAlpha::hypo_addcut(alpha_id_t target) {
+    return _hypo_addcut(&stageDelegate, target);
+}
+int QAlpha::hypo_remcut(alpha_id_t target) {
+    return _hypo_remcut(&stageDelegate, target);
+}
+
+int QAlpha::clip_insert(alpha_id_t target, alpha_id_t content) {
+    return _hypo_insert(&clipboardDelegate, target, content);
+}
+int QAlpha::clip_delete(alpha_id_t target) {
+    return _hypo_delete(&clipboardDelegate, target);
+}
+int QAlpha::clip_addcut(alpha_id_t target) {
+    return _hypo_addcut(&clipboardDelegate, target);
+}
+int QAlpha::clip_remcut(alpha_id_t target) {
+    return _hypo_remcut(&clipboardDelegate, target);
+}
--- qalpha.h
+++ qalpha.h
@@ -20,6 +20,9 @@ public:
     struct alpha_node *root;
 
     AlphaDelegate(alpha_id_t root_id);
+    ~AlphaDelegate();
+
+    void reset(void);
 
     alpha_id_t addNode(struct alpha_node *node);
     void addNode(struct alpha_node *node, alpha_id_t id);
@@ -34,6 +37,10 @@ public:
 class QAlpha : public QObject {
 
     Q_OBJECT
+    Q_PROPERTY(int root_id READ root_id)
+    Q_PROPERTY(int retOK READ retOK)
+    Q_PROPERTY(int retINVALID READ retINVALID)
+    Q_PROPERTY(int retMUST_PASTE READ retMUST_PASTE)
 
 private:
     struct alpha_node *stage_root;
@@ -41,26 +48,48 @@ private:
     AlphaDelegate stageDelegate;
     AlphaDelegate clipboardDelegate;
 
+    void _clear(AlphaDelegate *delegatep, struct alpha_node **rootp);
+    void _register(AlphaDelegate *delegatep, struct alpha_node *node);
+    QString _get_node(AlphaDelegate *delegatep, alpha_id_t id);
+    QList<int> _get_children(AlphaDelegate *delegatep, alpha_id_t id);
+    QList<int> _get_children(AlphaDelegate *delegatep, struct alpha_node *node);
+    QList<int> _get_descendants(AlphaDelegate *delegatep, alpha_id_t id);
+    QList<int> _get_descendants(AlphaDelegate *delegatep, struct alpha_node *node);
+
+    int _hypo_insert(AlphaDelegate *delegatep, alpha_id_t target, alpha_id_t content);
+    int _hypo_delete(AlphaDelegate *delegatep, alpha_id_t target);
+    int _hypo_addcut(AlphaDelegate *delegatep, alpha_id_t target);
+    int _hypo_remcut(AlphaDelegate *delegatep, alpha_id_t target);
+
 public:
     const alpha_id_t root_id = QALPHA_ROOT_ID;
+    const int retOK = 0;
+    const int retINVALID = -1;
+    const int retMUST_PASTE = 1;
+
     explicit QAlpha(QObject *parent = nullptr);
 
     Q_INVOKABLE void stage_clear(void);
     Q_INVOKABLE void clip_clear(void);
+    /* empty string => cut */
+    Q_INVOKABLE QString stage_get_node(alpha_id_t id);
+    Q_INVOKABLE QString clip_get_node(alpha_id_t id);
+    Q_INVOKABLE QList<int> stage_get_children(alpha_id_t id);
+    Q_INVOKABLE QList<int> clip_get_children(alpha_id_t id);
 
-    Q_INVOKABLE int prf_insert(alpha_id_t target, alpha_id_t);
+    Q_INVOKABLE int prf_insert(alpha_id_t target, alpha_id_t content);
     Q_INVOKABLE int prf_delete(alpha_id_t target);
     Q_INVOKABLE int prf_addcut(alpha_id_t target);
     Q_INVOKABLE int prf_remcut(alpha_id_t target);
     Q_INVOKABLE int prf_cut(alpha_id_t target);
     Q_INVOKABLE int prf_yank(alpha_id_t target);
 
-    Q_INVOKABLE int hypo_insert(alpha_id_t target, alpha_id_t);
+    Q_INVOKABLE int hypo_insert(alpha_id_t target, alpha_id_t content);
     Q_INVOKABLE int hypo_delete(alpha_id_t target);
     Q_INVOKABLE int hypo_addcut(alpha_id_t target);
     Q_INVOKABLE int hypo_remcut(alpha_id_t target);
 
-    Q_INVOKABLE int clip_insert(alpha_id_t target, alpha_id_t);
+    Q_INVOKABLE int clip_insert(alpha_id_t target, alpha_id_t content);
     Q_INVOKABLE int clip_delete(alpha_id_t target);
     Q_INVOKABLE int clip_addcut(alpha_id_t target);
     Q_INVOKABLE int clip_remcut(alpha_id_t target);
